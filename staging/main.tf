085ad108e66727e06de44dea8e42f844772dec60
JunYoung_Kim <hjklo0220@gmail.com>
2023-08-28 14:04:11 +0900

    feat: staging 환경 구성

--- staging/main.tf
+++ staging/main.tf
@@ -27,6 +27,75 @@ module "network" {
   env = local.env
 }
 
+module "be_server" {
+  source = "../modules/server"
+  NCP_ACCESS_KEY = var.NCP_ACCESS_KEY
+  NCP_SECRET_KEY = var.NCP_SECRET_KEY
+  env = local.env
+  name = "be"
+
+  init_script_path = "be_init_script.tftpl"
+  init_script_envs = {
+    password = var.password
+    DB_HOST = ncloud_public_ip.db.public_ip
+    POSTGRES_DB = var.POSTGRES_DB
+    POSTGRES_USER = var.POSTGRES_USER
+    POSTGRES_PASSWORD = var.POSTGRES_PASSWORD
+    POSTGRES_PORT = var.POSTGRES_PORT
+    DJANGO_SETTINGS_MODULE = var.DJANGO_SETTINGS_MODULE
+    DJANGO_SECRET_KEY = var.DJANGO_SECRET_KEY
+  }
+
+  server_image_product_code = data.ncloud_server_products.sm.server_image_product_code
+  product_code = data.ncloud_server_products.sm.product_code
+
+  vpc_no = module.network.vpc_no
+  subnet_no = module.network.main_subnet_no
+  acg_port_range = "8000"
+}
+
+module "db_server" {
+  source = "../modules/server"
+  NCP_ACCESS_KEY = var.NCP_ACCESS_KEY
+  NCP_SECRET_KEY = var.NCP_SECRET_KEY
+  env = local.env
+  name = "db"
+
+  init_script_path = "db_init_script.tftpl"
+  init_script_envs = {
+    password = var.password
+    POSTGRES_DB = var.POSTGRES_DB
+    POSTGRES_USER = var.POSTGRES_USER
+    POSTGRES_PASSWORD = var.POSTGRES_PASSWORD
+    POSTGRES_PORT = var.POSTGRES_PORT
+  }
+
+  server_image_product_code = data.ncloud_server_products.sm.server_image_product_code
+  product_code = data.ncloud_server_products.sm.product_code
+
+  vpc_no = module.network.vpc_no
+  subnet_no = module.network.main_subnet_no
+  acg_port_range = "5432"
+}
+
+module "lb" {
+  source = "../modules/lb"
+  NCP_ACCESS_KEY = var.NCP_ACCESS_KEY
+  NCP_SECRET_KEY = var.NCP_SECRET_KEY
+  env = local.env
+  lb_subnet_no = module.network.lb_subnet_no
+  vpc_no = module.network.vpc_no
+  be_instance_no = module.be_server.instance_no
+}
+
+
+resource "ncloud_public_ip" "be" {
+    server_instance_no = module.be_server.instance_no
+}
+
+resource "ncloud_public_ip" "db" {
+    server_instance_no = module.db_server.instance_no
+}
 
 data "ncloud_server_products" "sm" {
   server_image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR2004.B050"
