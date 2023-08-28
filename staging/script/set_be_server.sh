96af33718d8b123fa1a457a064bfbd3eec596eb6
JunYoung_Kim <hjklo0220@gmail.com>
2023-08-28 16:11:31 +0900

    feat: setting server script

---
+++ staging/script/set_be_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export (cat .env | xargs)
+
+# docker login
+docker login devops6th-cr.kr.ncr.ntruss.com \
+    -u "" \
+    -p ""
+
+# image pull
+docker pull devops6th-cr.kr.ncr.ntruss.com/lion-app:latest
+
+# docker run
+docker run -p 8000:8000 -d \
+    -v ~/.aws:/root/.aws:ro \
+    --env-file .env \
+    --name lion-app \
+    devops6th-cr.kr.ncr.ntruss.com/lion-app:latest
---
+++ staging/script/set_db_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# create volume
+# docker create volume postgres-data
+
+# docker run
+echo "docker run"
+docker run --name lion-db -d -p 5432:5432 \
+    --env-file .env \
+    -v postgres-data:/var/lib/postgresql/data \
+    postgres:13
