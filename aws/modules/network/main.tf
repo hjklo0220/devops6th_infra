4f1252f491171ec88fb3ddced4982ec13ad7f169
JunYoung_Kim <hjklo0220@gmail.com>
2023-08-29 14:41:28 +0900

    init: aws network module

---
+++ aws/modules/network/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "lion-vpc-${var.env}"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "lion-subnet-${var.env}"
+    Env = var.env
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route" "main" {
+  route_table_id            = aws_route_table.main.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+  depends_on                = [aws_route_table.main]
+}
